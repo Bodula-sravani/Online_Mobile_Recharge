96853e46da43db3ddd9560a3fef846b4bc328b90
unknown <chandrashanmugam007@gmail.com>
2023-04-23 19:28:40 +0530

    some minute editing on contact page UI

---
+++ online-recharge-app-query.sql
@@ -0,0 +1,5 @@
+﻿select * from AspNetRoles;
+select * from AspNetUsers;
+insert into AspNetRoles values('ABCD1234','Admin','Admin','2022-02-01');
+insert into AspNetUserRoles values('7e65bfbb-89b2-4816-b3e3-fe0eed3dc15a','ABCD1234');
+select * from AspNetUserRoles;
